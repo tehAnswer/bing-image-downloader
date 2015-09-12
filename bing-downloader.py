801bcc9034377288e8b41be3019af353e07968f3
Noé Fernández <UO227743@uniovi.es>
2015-09-12 02:33:33 +0200

    First version of the image downloader

---
+++ bing-downloader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import json
+import urllib
+import urllib2
+
+jsonResponse = json.loads(
+    urllib2.urlopen('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US').read())
+
+imageUrl = jsonResponse['images'][0]['url']
+
+# download the image and once it's downloaded, we will write it to an image
+imageUrl = "http://www.bing.com" + imageUrl
+imageFile = open("/home/noe/Pictures/bing-daily-backgorund.jpg", "wb")
+imageFile.write(urllib.urlopen(imageUrl).read())
+imageFile.close()
